89bcff57a0436307dcefbd0c18115dd5d1b17aa0
Priyobrato Das <priyodas12@gmail.com>
2020-12-24 23:01:17 +0530

    future

--- src/future/FutureImpl.java
+++ src/future/FutureImpl.java
@@ -1,19 +1,35 @@
 package future;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FutureImpl {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService= Executors.newFixedThreadPool(10);
 
-        Future<Integer> fval=executorService.submit(new Task());
+        List<Integer> futureList=new ArrayList<>();
+        //place holder for the object which will be returned from callable task
+        IntStream.range(0,10)
+                .forEach(i->{
+                    Future<Integer> fval=executorService.submit(new Task());
+                    try {
+                        futureList.add(fval.get(1, TimeUnit.SECONDS));
+                        System.out.println(Thread.currentThread().getName()+" prints "+fval.get());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (TimeoutException e) {
+                        e.printStackTrace();
+                    }
 
-        System.out.println(Thread.currentThread().getName()+" prints "+fval.get());
+                });
 
+        futureList.stream().forEach(System.out::println);
         executorService.shutdown();
     }
 }
--- src/future/Task.java
+++ src/future/Task.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Callable;
 public class Task implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
-        Thread.sleep(5000);
+        Thread.sleep(900);
         return new Random().nextInt();
     }
 }
